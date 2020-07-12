95823639fbbb3ca1e69a0606b7292f0e7255725f
Lamara Lanchava <58632996+lamchik@users.noreply.github.com>
2020-07-12 22:14:18 +0300

    поправить ошибки

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,13 @@ let nameInput = document.querySelector('.popup__name');
 let descriptionInput = document.querySelector('.popup__description');
 let name = document.querySelector('.profile__info-name');
 let popup = document.querySelector('.popup');
-let button = document.querySelector('.popup__button');
 let description = document.querySelector('.profile__info-description');
 let profileInfoEdit = document.querySelector('.profile__info-edit');
 let close = document.querySelector('.popup__close');
+let form = document.querySelector('.popup__wrapper-name-description');
 
-function saveTextOnSubmit(evt) {
+function saveText(evt) {
     evt.preventDefault()
-    saveText()
-}
-
-function saveTestOnEnter(e) {
-    if (e.code === 'Enter') {
-        saveText();
-    }
-};
-
-function saveText() {
     if (nameInput.value !== '') {
         name.textContent = nameInput.value;
     };
@@ -32,17 +22,13 @@ function addPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = name.textContent;
     descriptionInput.value = description.textContent;
-
-    button.addEventListener('click', saveTextOnSubmit);
-    document.addEventListener('keydown', saveTestOnEnter);
 };
 
 function closePopup() {
     popup.classList.remove('popup_opened');
-
-    button.removeEventListener('click', saveTextOnSubmit);
-    document.removeEventListener('keydown', saveTestOnEnter);
 };
 
 profileInfoEdit.addEventListener('click', addPopup);
 close.addEventListener('click', closePopup);
+
+form.addEventListener('submit', saveText)
